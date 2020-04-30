8511d9dad274e91e2aec6eae95eaac0286038b7c
Arjun Akkiraju <20arjuna@students.harker.org>
2020-04-29 18:13:30 -0700

    Got input from command line working

--- program1.java
+++ program1.java
@@ -1,26 +1,27 @@
 import java.util.Arrays;
 
-public class program1
+public class automaton
 {
+
+    public static<T> T[] subArray(T[] array, int beg, int end)
+    {
+        return Arrays.copyOfRange(array, beg, end);
+    }
+
+    public
     public static void main (String [] args)
     {
         int autoLength = Integer.parseInt(args[0]);
         int generations = Integer.parseInt(args[1]);
-        String [] temp = subArray(args, 1, args.length);
+        String [] temp = subArray(args, 2, args.length);
         int [] updateRule = new int[temp.length];
 
-        for(int i = 0; i < temp.length; i++)
+        for(int i = 0; i < updateRule.length; i++)
         {
             updateRule[i] = Integer.parseInt(temp[i]);
         }
 
-        System.out.println("autoLength: " + autoLength);
-        System.out.println("generations: " + generations);
-        System.out.println("updateRule: " + updateRule);
+        makeGen0(autoLength);
     }
 
-    public static<T> T[] subArray(T[] array, int beg, int end)
-    {
-        return Arrays.copyOfRange(array, beg, end + 1);
-    }
 }
